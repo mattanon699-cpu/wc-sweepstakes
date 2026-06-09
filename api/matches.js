eb2f21143a161961e49e731d54823e32957832f2
Eamo <mattanon699@gmail.com>
2026-06-10 00:23:46 +0100

    switch back to 2026

--- api/matches.js
+++ api/matches.js
@@ -1,7 +1,7 @@
 // v2
 export default async function handler(req, res) {
   const response = await fetch(
-    "https://api.football-data.org/v4/competitions/WC/matches?season=2022",
+    "https://api.football-data.org/v4/competitions/WC/matches?season=2026",
     { headers: { "X-Auth-Token": process.env.API_KEY } }
   );
   const data = await response.json();
